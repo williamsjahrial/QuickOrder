bf301a825f442c01dbb9440b97c6bd9b1280e790
William Sjahrial <will@willsjah.com>
2017-04-29 08:51:28 +0700

    Initial Commit

---
+++ QuickOrder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickOrder
+//
+//  Created by William Sjahrial on 4/29/17.
+//  Copyright © 2017 Mirai Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
